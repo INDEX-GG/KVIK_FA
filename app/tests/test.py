3d285a35fb62919510ce64b443faaab7965c92ff
Sergey Zinyaev <zinyaevsi@gmail.com>
2022-06-24 11:59:17 +0500

    tests

--- app/tests/test.py
+++ app/tests/test.py
@@ -1,17 +1,33 @@
 import unittest
+from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.api.v1.aggregator import api_router
+from app.core.config import settings
+from app.db.db_models import Base
 
 
-def square(n):
-    return n * n
+def start_application():
+    app = FastAPI(
+        root_path=settings.ROOT_PATCH,
+        title=settings.TITLE,
+        description=settings.DESCRIPTION,
+        version=settings.VERSION,
+        contact=settings.CONTACTS,
+        openapi_tags=settings.OPENAPI_TAGS
+    )
+    app.include_router(api_router)
+    return app
 
 
-def cube(n):
-    return n * n * n
+SQLALCHEMY_DATABASE_URL = settings.TESTS_SQLALCHEMY_DATABASE_URL
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionTesting = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+Base.metadata.drop_all(bind=engine)
 
 
 class Test(unittest.TestCase):
     def test_square(self):
-        self.assertEquals(square(2), 4)
-
-    def test_cube(self):
-        self.assertEquals(cube(2), 8)
+        self.assertEquals(4, 4)
