cc77d6833bd98538373f31bb3ba39c47656c1e2c
Sergey Zinyaev <zinyaevsi@gmail.com>
2022-07-04 15:06:15 +0500

    refactoring

--- app/api/v1/aggregator.py
+++ app/api/v1/aggregator.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import users, phone, login, categories
+from app.api.v1.endpoints import users, phone, login, categories, post
 
 api_router = APIRouter(prefix="/v1")
 api_router.include_router(users.router)
 api_router.include_router(phone.router)
 api_router.include_router(login.router)
 api_router.include_router(categories.router)
+api_router.include_router(post.router)
